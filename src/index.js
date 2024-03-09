6f126416d977585c54028648d606207e33f38152
annisssimo <anyalar04@gmail.com>
2024-03-09 19:39:11 +0300

    feat: header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
 import WebFont from 'webfontloader';
+import Task from './task.js';
 
 WebFont.load({
   google: {
     families: ['Rubik Doodle Shadow:400'],
   },
 });
+
---
+++ src/task.js
@@ -0,0 +1,9 @@
+export class Task {
+    constructor(title, description, dueDate, priority) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+    }
+}
+
