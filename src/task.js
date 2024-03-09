48e3aa2727513c89381ceffc722c099be7e39245
annisssimo <anyalar04@gmail.com>
2024-03-09 20:37:26 +0300

    feat: install date-fns

--- src/task.js
+++ src/task.js
@@ -1,12 +1,14 @@
+import {parse} from 'date-fns';
+
 class Task {
     constructor(title, description, dueDate, priority) {
       this.title = title;
       this.description = description;
-      this.dueDate = dueDate;
+      this.dueDate = parse(dueDate, 'dd.MM.yyyy', new Date());
       this.priority = priority;
     }
 }
 
-const task1 = new Task('clean the home', 'with Yarik', '09.03.2024', 'low');
+const task1 = new Task('clean the home', 'with Yarik', '08.03.2024', 'low');
 
 export default task1;
